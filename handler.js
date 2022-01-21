d7a4b61ba3d71b7602f2a89f8f84299fc5dd969e
Sai Nimmagadda <saiguy@Sais-Mac-mini.attlocal.net>
2022-01-21 17:24:58 -0500

    Add lambda permissions to invoke

--- handler.js
+++ handler.js
@@ -4,10 +4,8 @@ const { retweet } = require('./helpers/twitter');
 const { scheduleInvocation } = require('./helpers/scheduler');
 
 module.exports.bot = async (event, context, callback) => {
-
   console.log("retweeting")
   retweet()
   await scheduleInvocation();
   console.log("finished")
-
 };
--- helpers/scheduler.js
+++ helpers/scheduler.js
@@ -1,10 +1,9 @@
 const AWS = require("aws-sdk");
-
 const RULE_NAME = 'GarlicTweet';
 const LAMBDA_ARN = process.env.LAMBDA_ARN;
 
 async function scheduleInvocation() {
-    const nextInterval = between(60, 240);
+    const nextInterval = between(100, 240);
     const currentTime = new Date().getTime(); // UTC Time
     const nextTime = addMins(currentTime, nextInterval);
     const nextMinutes = nextTime.getMinutes();
@@ -23,6 +22,10 @@ async function scheduleInvocation() {
 
     const targetResponse = await addTarget(cloudEvent);
     console.log('##### target response ', targetResponse);
+
+    // Last tell AWS to trust invocations of lambda from cloudwatch
+    const lambdaPermission = await addPerm(targetResponse.RuleArn);
+    console.log('##### attached permissions ', lambdaPermission);
 }
 
 function between(min, max) {
@@ -56,7 +59,7 @@ const addTarget = (cloudEvent) => {
         Targets: [
             {
                 Arn: LAMBDA_ARN,
-                Id: 'random-id-' + Math.random(),
+                Id: 'random-id-1234'
             }
         ]
     }
@@ -65,6 +68,23 @@ const addTarget = (cloudEvent) => {
         cloudEvent.putTargets(params, (err, data) => {
             if (err) reject(err)
             else resolve(data)
+        });
+    });
+}
+
+async function addPerm(source) {
+    const params = {
+        Action: "lambda:InvokeFunction",
+        FunctionName: LAMBDA_ARN,
+        Principal: "events.amazonaws.com",
+        SourceArn: source,
+        StatementId: "tweet-scheduled-event"
+    };
+    const lambda = new AWS.Lambda();
+    return new Promise((resolve, reject) => {
+        lambda.addPermission(params, function (err, data) {
+            if (err) reject(err, err.stack); // an error occurred
+            else resolve(data);           // successful response
         });
     });
 }
